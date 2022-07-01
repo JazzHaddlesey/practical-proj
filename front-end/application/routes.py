8b679a60345e641b80ae1b02398f8ac50d12e96e
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-07-01 11:25:59 +0000

    JH: added to templates

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -4,7 +4,7 @@ import requests
 
 @app.route('/')
 def index():
-    name = requests.get('http://rand-name-gen:5000/get_name').text
+    full_name = requests.get('http://rand-name-gen:5000/get_name').text
     nums = requests.get('http://rand-num-gen:5000/get_num').text
     race = requests.post('http://race-gen:5000/race', json = dict(nums = nums))
     return render_template('home.html', race = race.text)
