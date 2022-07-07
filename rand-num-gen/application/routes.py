e968603c8c1edf3756eea83e5f976759970c0538
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-07-07 17:36:10 +0000

    JH: added stuff for rolling update

--- rand-num-gen/application/routes.py
+++ rand-num-gen/application/routes.py
@@ -3,5 +3,6 @@ from random import randint
 
 @app.route('/get_nums', methods = ['GET'])
 def get_nums():
-    nums = randint(1, 179)
+    nums = randint(1, 179) 
+    # nums = randint(1, 279)
     return str(nums)
